281da217e1fbfbc19b41fa0e951ebd51ea4845be
AndreasSjo <andreas.sjoden@hotmail.com>
2022-03-31 17:03:57 +0200

    add andreas webtest

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,37 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class SeleniumTest {
+
+    @Test
+    public void andreasTest() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver webDriver = new ChromeDriver();
+
+        webDriver.get("https://www.imdb.com/");
+
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
+        Thread.sleep(5*1000);
+
+        String siteName = webDriver.getTitle();
+        System.out.println(siteName);
+
+        WebElement searchBox = webDriver.findElement(By.id("suggestion-search"));
+        WebElement searchButton = webDriver.findElement(By.id("suggestion-search-button"));
+
+        searchBox.sendKeys("Tillbaka till framtiden");
+        searchButton.click();
+
+        String title = webDriver.findElement(By.className("result_text")).toString();
+
+
+
+    }
+}
